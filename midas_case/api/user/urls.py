a60203b32e61edad5167cca385819807224bc0ea
goktugbc <e188139@metu.edu.tr>
2023-03-02 12:30:56 +0300

    Login, logout endpoints

--- midas_case/api/user/urls.py
+++ midas_case/api/user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import Register
+from .views import Register, Login, Logout
 
 urlpatterns = [
     path('users/register', Register.as_view(), name='register'),
+    path('users/login', Login.as_view(), name='login'),
+    path('users/logout', Logout.as_view(), name='logout'),
     #path('users/register', RetrieveUser.as_view(), name='retrieve_user'),
 ]
--- midas_case/api/user/views.py
+++ midas_case/api/user/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import authenticate, login, logout
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.response import  Response
+from rest_framework.response import Response
+from midas_case.rest_framework_settings import CsrfExemptSessionAuthentication
 from .serializers import RegistrationSerializer
 
 
@@ -11,3 +13,24 @@ class Register(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Login(APIView):
+    authentication_classes = (CsrfExemptSessionAuthentication,)
+    def post(self, request):
+        if 'username' not in request.data or 'password' not in request.data:
+            return Response({'msg': 'Credentials missing'}, status=status.HTTP_400_BAD_REQUEST)
+        username = request.data['username']
+        password = request.data['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return Response({'msg': 'Login Success'}, status=status.HTTP_200_OK)
+        return Response({'msg': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class Logout(APIView):
+    authentication_classes = (CsrfExemptSessionAuthentication,)
+    def post(self, request):
+        logout(request)
+        return Response({'msg': 'Successfully Logged out'}, status=status.HTTP_200_OK)
